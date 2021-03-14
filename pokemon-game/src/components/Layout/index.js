4efd459fc6f7d0ca4c706f883d5042b378862dfa
KarinaLogvina <karinalogvina0@gmail.com>
2021-03-14 13:14:57 +0300

    refactor: tabs;

--- pokemon-game/src/components/Layout/index.js
+++ pokemon-game/src/components/Layout/index.js
@@ -1,10 +1,10 @@
 import s from './layout.module.css';
 
-const Layout = ({title, urlBg, colorBg, colorTitle, children}) => {
+const Layout = ({ title, urlBg, colorBg, colorTitle, children }) => {
     const style = {};
     if (urlBg) { style.backgroundImage = `url(${urlBg})` }
     if (colorBg) { style.backgroundColor = colorBg }
-    
+
     return (
         <section className={s.root} style={style}>
             <div className={s.wrapper}>
