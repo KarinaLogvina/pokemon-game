2cfa8cb631ff0065f915e7b7dc07a93e45b2e84f
KarinaLogvina <karinalogvina0@gmail.com>
2021-03-22 19:42:56 +0300

    refactor: card active;

--- pokemon-game/src/components/routes/Game/index.js
+++ pokemon-game/src/components/routes/Game/index.js
@@ -5,35 +5,74 @@ import PokemonCard from '../../PokemonCard';
 
 import s from './game.module.css'
 
+const DATA = {
+    "abilities": [
+        "keen-eye",
+        "tangled-feet",
+        "big-pecks"
+    ],
+    "base_experience": 122,
+    "height": 11,
+    "weight": 300,
+    "id": 17,
+    "img": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/17.png",
+    "name": "pidgeotto",
+    "stats": {
+        "hp": 63,
+        "attack": 60,
+        "defense": 55,
+        "special-attack": 50,
+        "special-defense": 50,
+        "speed": 71
+    },
+    "type": "normal",
+    "values": {
+        "top": 7,
+        "right": 5,
+        "bottom": 1,
+        "left": 2
+    }
+}
+
 const GamePage = () => {
-    const [allPokemons, setAllPokemons] = useState([]);
+    const [pokemons, setPokemons] = useState([]);
 
-    useEffect(() => {
+    const getPokemons = () => {
         database.ref('pokemons').once('value', (snapshot) => {
-            setAllPokemons(snapshot.val())
+            setPokemons(snapshot.val())
         });
-    }, [])
-
-    const setPokemonActive = (key) => {
-        return database.ref('pokemons/' + key).update(
-            { isActive: !allPokemons[key].active }
-        )
-            .then(() => {
-                allPokemons[key].active = !allPokemons[key].active
-            })
     }
 
-    const createPokemon = () => {
-        const keyId = database.ref().child('polemons').push().key;
-        database.ref('pokemons/' + keyId).update({ isActive: !keyId.active })
+    useEffect(() => {
+        getPokemons();
+    }, [])
+
+    const handleChangeActive = (id) => {
+        setPokemons(prevState => {
+            return Object.entries(prevState).reduce((acc, item) => {
+                const pokemon = { ...item[1] };
+                if (pokemon.id === id) {
+                    pokemon.active = !pokemon.active
+                }
+                acc[item[0]] = pokemon;
+                database.ref('pokemons/' + item[0]).set(pokemon)
+                return acc;
+            }, {})
+        });
+    }
+
+    const handleAddPokemon = () => {
+        const data = DATA;
+        const newKey = database.ref().child('pokemons').push().key;
+        database.ref('pokemons/' + newKey).set(data).then(() => getPokemons());
     }
 
     return (
         <>
-            <div><button onClick={createPokemon}>Create new pokemon</button></div>
+            <div><button onClick={handleAddPokemon}>Create new pokemon</button></div>
             <div className={s.flex}>
                 {
-                    Object.entries(allPokemons).map(([key, { id, name, img, type, values, active }]) => <PokemonCard
+                    Object.entries(pokemons).map(([key, { id, name, img, type, values, active }]) => <PokemonCard
                         isActive={active}
                         name={name}
                         id={id}
@@ -41,7 +80,7 @@ const GamePage = () => {
                         type={type}
                         values={values}
                         key={key}
-                        setPokemonActive={() => setPokemonActive(key)}
+                        setPokemonActive={handleChangeActive}
                     />)
                 }
             </div>
