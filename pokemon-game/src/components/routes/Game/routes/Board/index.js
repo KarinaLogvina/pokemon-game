f88ff4279a3bca7e741c867fd5e359b9f65d4998
KarinaLogvina <karinalogvina0@gmail.com>
2021-03-25 20:27:14 +0300

    feat: player two;

--- pokemon-game/src/components/routes/Game/routes/Board/index.js
+++ pokemon-game/src/components/routes/Game/routes/Board/index.js
@@ -1,22 +1,39 @@
-import { useContext } from 'react';
-import { useHistory } from 'react-router-dom';
+import { useContext, useEffect, useState } from 'react';
+// import { useHistory } from 'react-router-dom';
 import { PokemonContext } from '../../../../context/pokemonContext';
 import PokemonCard from '../../../../PokemonCard';
 import s from './style.module.css';
 
 const BoardPage = () => {
-    const { pokemons } = useContext(PokemonContext);
-    const history = useHistory();
+    const [board, setBoard] = useState([]);
+    const [player2, setPlayer2] = useState([])
+    const { pokemon } = useContext(PokemonContext);
 
-    if (Object.keys(pokemons).length === 0) {
-        history.replace('/game')
+    // const history = useHistory();
+
+    // if (Object.keys(pokemons).length === 0) {
+    //     history.replace('/game')
+    // }
+
+    const handleClickBoardPlate = (position) => {
+
     }
+
+    useEffect(async () => {
+        const boardResponse = await fetch('https://reactmarathon-api.netlify.app/api/board');
+        const boardRequest = await boardResponse.json();
+        setBoard(boardRequest.data);
+
+        const player2Response = await fetch('https://reactmarathon-api.netlify.app/api/create-player');
+        const player2Request = await player2Response.json();
+        setPlayer2(player2Request.data);
+    }, [])
 
     return (
         <div className={s.root}>
             <div className={s.playerOne}>
                 {
-                    Object.values(pokemons).map(({ id, name, img, minimize, type, values }) => (
+                    Object.values(pokemon).map(({ id, name, img, minimize, type, values }) => (
                         <PokemonCard
                             className={s.card}
                             isActive={true}
@@ -32,15 +49,37 @@ const BoardPage = () => {
                 }
             </div>
             <div className={s.board}>
-                <div className={s.boardPlate}>1</div>
-                <div className={s.boardPlate}>2</div>
-                <div className={s.boardPlate}>3</div>
-                <div className={s.boardPlate}>4</div>
-                <div className={s.boardPlate}>5</div>
-                <div className={s.boardPlate}>6</div>
-                <div className={s.boardPlate}>7</div>
-                <div className={s.boardPlate}>8</div>
-                <div className={s.boardPlate}>9</div>
+                {
+                    board.map((item) => (
+                        <div
+                            key={item.position}
+                            className={s.boardPlate}
+                            onClick={() => item.card && handleClickBoardPlate(item.position)}
+                        >
+                            {
+                                item.card && <PokemonCard {...item} minimize />
+                            }
+                        </div>
+
+                    ))
+                }
+            </div>
+            <div className={s.playerTwo}>
+                {
+                    player2.map(({ id, name, img, minimize, type, values }) => (
+                        <PokemonCard
+                            className={s.card}
+                            isActive={true}
+                            name={name}
+                            id={id}
+                            img={img}
+                            type={type}
+                            values={values}
+                            minimize
+                            isActive
+                        />
+                    ))
+                }
             </div>
         </div>
     );
--- pokemon-game/src/components/routes/Game/routes/Start/index.js
+++ pokemon-game/src/components/routes/Game/routes/Start/index.js
@@ -53,7 +53,7 @@ const StartPage = () => {
                             key={key}
                             isSelected={selected}
                             setPokemonActive={() => {
-                                if (Object.keys(pokemonContext.pokemons).length < 5 || selected) {
+                                if (Object.keys(pokemonContext.pokemon).length < 5 || selected) {
                                     handleChangeSelected(key)
                                 }
                             }}
@@ -63,7 +63,7 @@ const StartPage = () => {
             <div>
                 <button
                     onClick={handleStartGame}
-                    disabled={Object.keys(pokemonContext.pokemons).length < 5}
+                    disabled={Object.keys(pokemonContext.pokemon).length < 5}
                 >
                     Start</button>
             </div>
